f222c2980d53de90c3b596e6cbedb8727b06cb45
Mohammed Hany <mohammedhany300@gmail.com>
2023-02-17 01:44:09 -0800

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+/**
+ * main - Entry point
+ *
+ * Return: Size of some data types and 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)", sizeof(char);
+	printf("Size of an int: %d byte(s)", sizeof(int;
+	printf("Size of a long int: %d byte(s)", sizeof(long int);
+	printf("Size of a long long: %d byte(s)", sizeof(long long int);
+
+	return (0);
+}
