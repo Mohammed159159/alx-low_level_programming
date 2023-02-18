4a440dea53077062440d1eb3ed7d917567f71c87
Mohammed Hany <mohammedhany300@gmail.com>
2023-02-18 13:54:17 -0800

    dev: Solve comb 4

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Comma-separated list of numbers and 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i <= 57; i++)
+	{
+
+		for (j = i + 1; j <= 57; j++)
+		{
+			for (k = i; k <= 57; k++)
+			{
+
+				for (l = j + 1; l <= 57; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					putchar(l);
+					if (i != 57 || j != i - 1 || k != i + 2 || l != k)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
