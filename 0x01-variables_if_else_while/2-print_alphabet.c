0f83ae6ba211c9b74e382ff1508fe58a47f78dd8
Mohammed Hany <mohammedhany300@gmail.com>
2023-02-18 11:38:59 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alphabet in lower casse and 0 (Success)
+ */
+
+int main(void)
+{
+	for (int i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+
+	putchar("\n");
+}
