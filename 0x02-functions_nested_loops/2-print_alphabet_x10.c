b04a90490ad4d918a2c3301555ec43b2f04d7e4e
Mohammed Hany <mohammedhany300@gmail.com>
2023-02-26 14:40:06 -0800

    Commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,6 +8,7 @@ void print_alphabet_x10(void)
 {
 	char alph;
 	int i;
+	
 	for (i = 0; i < 10; i++)
 	{
 
@@ -17,5 +18,5 @@ void print_alphabet_x10(void)
 			_putchar(alph);
 		}
 		_putchar('\n');
-	}	
+	}
 }
