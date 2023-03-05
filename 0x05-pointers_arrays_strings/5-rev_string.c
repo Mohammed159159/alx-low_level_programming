3bfe1da33d40a9984b0a2446ab432d2e15f83f12
Mohammed Hany <mohammedhany300@gmail.com>
2023-03-05 09:17:58 -0800

    Commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,19 +9,19 @@
 void rev_string(char *s)
 {
 	int i, j;
-        char *a, *b;
+	char *a, *b;
 
-        for (i = 0; *(s + i) != '\0'; i++)
-        ;
+	for (i = 0; *(s + i) != '\0'; i++)
+	;
 
-        for (j = 0; j <= i / 2 - 1; j++)
-        {
-            a = s + j;
-            b = s + i - 1 - j;
+	for (j = 0; j <= i / 2 - 1; j++)
+	{
+		a = s + j;
+		b = s + i - 1 - j;
 
-           *a = *a ^ *b;
-           *b = *a ^ *b;
-           *a = *a ^ *b;
-        }
+		*a = *a ^ *b;
+		*b = *a ^ *b;
+		*a = *a ^ *b;
+	}
 
 }
