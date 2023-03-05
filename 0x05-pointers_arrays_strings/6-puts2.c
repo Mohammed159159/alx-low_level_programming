1826434e4fa00519ff03217ebf303c40f4adb476
Mohammed Hany <mohammedhany300@gmail.com>
2023-03-05 09:22:29 -0800

    Commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts2 - print every other charachter in a string from 1st charachter
+ *
+ * @str: pointer to string
+ *
+ * Return: void
+ */
+
+void _puts2(char *str)
+{
+	char *ch;
+
+	for (ch = str; *ch != '\0'; ch+=2)
+	{
+		_putchar(*ch);
+	}
+	_putchar('\n');
+}
