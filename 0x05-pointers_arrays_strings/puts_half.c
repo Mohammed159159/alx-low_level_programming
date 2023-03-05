fc3bdee758618b0e084f5332f35c01cd2e542954
Mohammed Hany <mohammedhany300@gmail.com>
2023-03-05 11:07:55 -0800

    Commit

---
+++ 0x05-pointers_arrays_strings/puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - print second half of a string
+ *
+ * @str: String to be used
+ *
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int i, n;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	;
+
+	if (i % 2 == 0)
+		n = i / 2;
+	else
+		n = (i - 1) / 2;
+
+	for (i = i - n; i >= 0; i--)
+	       _putchar(*(str + i));	
+
+}
