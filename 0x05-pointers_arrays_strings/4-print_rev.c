833db3cc74308a536e53b5644f294b3c8a74eccb
Mohammed Hany <mohammedhany300@gmail.com>
2023-03-05 07:15:54 -0800

    Commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,9 +13,9 @@ void print_rev(char *s)
 	int i;
 
 	for (i = 0; *(s + i) != '\0'; i++)
-	while (i-- >= 0)
+	while (i-- > 0)
 	{
-		_putchar(*(s + i - 1));
+		_putchar(*(s + i));
 	}
 	_putchar('\n');
 }
