6f0ff31d09ec2966c4d29dc0ad539784ca70ff15
Mohammed Hany <mohammedhany300@gmail.com>
2023-03-05 04:40:59 -0800

    Commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * _puts - print string
- * 
+ *
  * @str: pointer to string
  *
  * Return: void
@@ -11,10 +11,10 @@
 void _puts(char *str)
 {
 	char *ch;
-	
+
 	for (ch = str; *ch != '\0'; ch++)
 	{
 		_putchar(*ch);
 	}
-	_putchar('\n');	
+	_putchar('\n');
 }
