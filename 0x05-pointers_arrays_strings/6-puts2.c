36df9e90b0cd2d087903dbd6264238fee94dcd29
Mohammed Hany <mohammedhany300@gmail.com>
2023-03-05 10:13:58 -0800

    Commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts2 - print every other charachter in a string from 1st charachter
+ * puts2 - print every other charachter in a string from 1st charachter
  *
  * @str: pointer to string
  *
@@ -12,7 +12,7 @@ void puts2(char *str)
 {
 	char *ch;
 
-	for (ch = str; *ch != '\0'; ch++)
+	for (ch = str; *ch != '\0'; ch+=2)
 	{
 		_putchar(*ch);
 	}
