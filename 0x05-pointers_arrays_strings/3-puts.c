b4d32e273e03583a68c05665b8f3963543a330b3
Mohammed Hany <mohammedhany300@gmail.com>
2023-03-05 04:38:07 -0800

    Commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print string
+ * 
+ * @str: pointer to string
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	char *ch;
+	
+	for (ch = str; ch != '\0'; ch++)
+	{
+		_putchar(*ch);
+	}	
+}
