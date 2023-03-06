96b1ae4db6c743a548ed8f9365675dfeb23c2da2
Mohammed Hany <mohammedhany300@gmail.com>
2023-03-06 12:41:06 -0800

    Commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * _strstr - find first occurence of substring in a string
+ *
+ * @haystack: Pointer to string
+ * @needle: Pointer to subbstring
+ *
+ * Return: Pointer to beginning of substring, otherwise NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, j;
+	int isFound = 0;
+
+	for (i = 0; *(haystack + i) != '\0'; i++)
+	{
+		if (*(haystack + i) == *needle)
+		{
+			isFound = 1;
+			for (j = 1; *(needle + j) != '\0'; j++)
+			{
+				if (*(s + j) != *(needle + j))
+				{
+					isFound = 0;
+					break;
+				}
+			}
+
+			if (isFound)
+				return (s + i);
+		}
+	}
+
+	return (NULL);
+}
