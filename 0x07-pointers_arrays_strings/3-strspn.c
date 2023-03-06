2da8af4f9595ba1edb878402c3b7cd3ea169f9f7
Mohammed Hany <mohammedhany300@gmail.com>
2023-03-06 11:35:33 -0800

    Commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,17 +9,16 @@
  */
 unsigned int *_strspn(char *s, char *accept)
 {
-	unsigned int i;
-	unsigned int j;
+	unsigned int i, j, yes = 0;
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		for (j = 0; *(accept + j) != '\0'; j++)
 		{
 			if (*(s + i) == *(accept + j))
-				return (s + i);
+				yes++;
 		}
 	}
 
-	return (NULL);
+	return (yes);
 }
