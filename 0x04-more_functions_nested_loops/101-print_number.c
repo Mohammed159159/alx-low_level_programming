2589e782a1bc77a2abd50327181198053b2f2cd0
Mohammed <mohammedhany300gmail.com>
2023-09-16 01:04:28 +0300

    dev: print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - print an integer
+ * @n: integer to be printed
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	int i, last_digit;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	for (i = 1; i < n / 10; i *= 10)
+		;
+	for (; i > 0; i /=  10)
+	{
+		last_digit = n / i % 10;
+		_putchar(last_digit + 48);
+	}
+}
+
