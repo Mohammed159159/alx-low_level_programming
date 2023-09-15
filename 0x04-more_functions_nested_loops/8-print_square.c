031fa99e7ab808001e1bd295b8d4ef068f15838d
Mohammed <mohammedhany300gmail.com>
2023-09-15 20:18:13 +0300

    fix: Hot fix

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,6 +9,7 @@ void print_square(int size)
 {
 	int i, j;
 
+	if (size > 0)
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
@@ -18,4 +19,6 @@ void print_square(int size)
 
 		_putchar('\n');
 	}
+	else
+		_putchar('\n');
 }
