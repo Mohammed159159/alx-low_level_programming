fc8d376284cbecd4653ea00518fd950dd2563f1b
Mohammed <mohammedhany300gmail.com>
2023-09-15 20:55:12 +0300

    fix: Hot fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,6 +9,7 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
+	if (size > 0)
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - 1 - i; j++)
@@ -23,4 +24,6 @@ void print_triangle(int size)
 
 		_putchar('\n');
 	}
+	else
+		_putchar('\n');
 }
