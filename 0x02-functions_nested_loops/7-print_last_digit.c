c61eed51e0b3d8ca84ea2cad063b884282d8ad85
Mohammed <mohammedhany300gmail.com>
2023-09-17 17:23:00 +0300

    fix: Hot fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,5 +7,17 @@
  */
 int print_last_digit(int n)
 {
-	_putchar(n % 10 + '0');
+	int last_digit;
+
+	if (n < 0)
+		n = -n;
+
+	last_digit = n;
+
+	if (n > 9)
+		last_digit = n % 10;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
