81f7202f6b9f20598b3fe8059acdb1719dd101ab
Mohammed <mohammedhany300gmail.com>
2023-09-17 19:32:25 +0300

    fix: Hot fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,6 +11,7 @@ void print_times_table(int n)
 	int i, j, next;
 
 	if (n <= 15 && n > 0)
+	{
 		for (i = 0; i < n + 1; i++)
 		{
 			for (j = 0; j < n + 1; j++)
@@ -27,5 +28,6 @@ void print_times_table(int n)
 					printf(",   ");
 			}
 			printf("\n");
-		};
+		}
+	}
 }
