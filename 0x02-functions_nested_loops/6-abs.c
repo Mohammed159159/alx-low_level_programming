fe7a79d242992fd4d4373295bf6d52ccc152569c
Mohammed <mohammedhany300gmail.com>
2023-09-17 17:05:25 +0300

    dev: _abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+/**
+ * _abs - compute the absolute value of an integer
+ * @n - value to be computed
+ * Return: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	if (n < 0)
+		return (-n);
+}
