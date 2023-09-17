a87914912849c1f59f853b202589392f1df1854c
Mohammed <mohammedhany300gmail.com>
2023-09-17 18:08:30 +0300

    dev: times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * time_table - print the 0 times table, starting with 0
+ *
+ * Return: Nothing
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			printf("%d", i * j);
+
+			if (j == 9)
+				printf("\n");
+			else
+				printf(" ,");
+		}
+	}
+}
