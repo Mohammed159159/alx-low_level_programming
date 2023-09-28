d95675c8d5bb45a656a43f1b096d819d59d54fb9
Mohammed <mohammedhany300gmail.com>
2023-09-28 21:48:42 +0300

    dev: is_prime_number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,12 +1,29 @@
 /**
+ * check - checks if a number is prime against a certain divisor
+ * @x: number to be tested
+ * @d: testing divisor
+ * Return: 1 if prime, 0 otherwise
+ */
+int check(int x, int d)
+{
+	if (d * d > x)
+		return (1);
+
+	if (x % d == 0)
+		return (0);
+
+	d++;
+	return (check(x, d));
+}
+/**
  * is_prime_number - checks if a number is prime
  * @n: number to be checked
  * Return: 1 if the number is prime, 0 otherwise
  */
 int is_prime_number(int n)
 {
-	if (n == 2)
-		return (1);
-	
-	is_prime_number(n - 2)
+	if (n <= 2)
+		return (0);
+
+	return (check(n, 2));
 }
