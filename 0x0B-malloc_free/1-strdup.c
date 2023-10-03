84f74447b0e9e7c6aaa689dd2d8f6afc1b388957
Mohammed <mohammedhany300gmail.com>
2023-10-03 21:53:38 +0300

    fix: Hot fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,6 @@
 #include<stdlib.h>
 #include<stddef.h>
-
+#include<string.h>
 /**
  * _strdup - duplicate a string in a new memory space
  * @str: string to be duplicated
@@ -9,18 +9,18 @@
 char *_strdup(char *str)
 {
 	char *dup;
-	unsigned int i;
+	unsigned int i = strlen(str);
 
 	if (str == NULL)
 		return (NULL);
 	
-	dup = malloc(sizeof(str));
+	dup = malloc(i + 1);
 
 	if (dup == NULL)
 		return (NULL);
 
-	for (i = 0; i < sizeof(str); i++)
+	while(i--)
 		dup[i] = str[i];
-
+	
 	return (dup);
 }
