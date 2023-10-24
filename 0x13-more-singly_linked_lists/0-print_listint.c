237eaebd2f15ee8107d2595a1263be5130a48be7
Mohammed <mohammedhany300gmail.com>
2023-10-24 05:15:19 +0300

    dev: print_listint

---
+++ 0x13-more-singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "list.h"
+
+/**
+ * print_listint - print all elements of a singly linked list
+ * @h: pointer to the first element of the linked list
+ * Return: number of elements
+ */
+size_t print_listint(const linstint_t *h)
+{
+	int i;
+
+	for (i = 0; h; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (i);
+}
