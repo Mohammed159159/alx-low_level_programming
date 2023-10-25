501453e19e92cb62caf452856040e8ba6fb5a4bb
Mohammed <mohammedhany300gmail.com>
2023-10-25 04:48:03 +0300

    fix: Hot fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	for (i = 0; i < idx; i++)
 	{
-		*head = *(head)->next;
+		*head = (*head)->next;
 
 		if (!*head)
 		{
@@ -41,8 +41,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 	}
 
-	new_node->next = *(head)->next;
-	*(head)->next = new_node;
+	new_node->next = (*head)->next;
+	(*head)->next = new_node;
 	*head = h;
 
 	return (new_node);
