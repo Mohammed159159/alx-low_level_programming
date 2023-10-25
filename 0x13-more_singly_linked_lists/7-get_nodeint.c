3cf8853fc9df319a43712b7826b3e44b94c7f127
Mohammed <mohammedhany300gmail.com>
2023-10-25 04:15:03 +0300

    fix: Hot fix

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,16 +11,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (!head)
-		return (NULL);
-
-	for (i = 0; i < index; i++)
-	{
+	for (i = 0; head && i < index; i++)
 		head = head->next;
-
-		if (!head)
-			return (NULL);
-	}
 
 	return (head);
 }
